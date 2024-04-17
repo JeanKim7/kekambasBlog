83a192bc57baf8fafb1770c6882badca616ac227
JeanKim7 <jean.g.km@gmail.com>
2024-04-17 09:24:38 -0700

    Done with day2

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type UserType = {
+    id:number,
+    firstName:string,
+    lastName:string,
+    username:string,
+    email:string,
+    dateCreated:string
+}
+export type PostType = {
+    id:number,
+    title:string,
+    body:string,
+    dateCreated:string,
+    author:UserType
+}
+
+export type PostFormDataType = {
+    title:string,
+    body:string
+}
